32cc4c6e980f5ca2804ff3fe9f9093dba79cc055
Djoudi <djosamir@yahoo.fr>
2025-04-14 17:45:59 +0200

    reset front

--- screens/Connexion_inscription.js
+++ screens/Connexion_inscription.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { Button, StyleSheet, View, Text } from "react-native";
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-
 export default function ConnexionInscriptionScreen({ navigation }) {
-  
   const clientId = process.env.GOOGLE_CLIENT_ID;
-  
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Connexion</Text>
@@ -26,29 +21,6 @@ export default function ConnexionInscriptionScreen({ navigation }) {
         title="Inscription"
         onPress={() => navigation.navigate("Inscription")}
       />
-
-      <GoogleOAuthProvider clientId={clientId}>
-      <GoogleLogin
-              onSuccess={(credentialResponse) => {
-                //console.log(credentialResponse.credential);
-                const token = credentialResponse.credential;
-                const decoded = jwtDecode(token);
-                
-                console.log(decoded);
-                //console.log(decoded.name);
-                //console.log(decoded.email);
-                //console.log(decoded.picture);
-                //setUser({ name: decoded.name, email: decoded.email });
-              }}
-              //clientId
-              //credential
-              onError={() => {
-                console.log("Login Failed");
-              }}
-            />
-
-      </GoogleOAuthProvider>
-
     </View>
   );
 }
