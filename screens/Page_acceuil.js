d00dbbc77c4296a0afdf4fb73446723db9e7aa18
Demba <dembouyz@hotmail.com>
2025-04-15 11:54:00 +0200

    Sauvegarde

--- screens/Page_acceuil.js
+++ screens/Page_acceuil.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TouchableOpacity, View, SafeAreaView } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Article from './Article';
 
