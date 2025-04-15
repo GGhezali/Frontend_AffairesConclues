7e533b8f7afae715dcab6c0419f30340296711b4
GGhezali <gregory.ghezali@hotmail.fr>
2025-04-15 11:29:08 +0200

    new commit

--- screens/Page_acceuil.js
+++ screens/Page_acceuil.js
@@ -1,69 +1,84 @@
-import React, { useCallback, useState } from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function PageAcceuilScreen({ navigation }) {
   const [categorie, setCategorie] = useState(false);
   const [tri, setTri] = useState(false);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   const toggleCategorie = () => {
-    setCategorie(!categorie)
+    setCategorie(!categorie);
   };
   const toggleTri = () => {
-    setTri(!tri)
+    setTri(!tri);
   };
-  const selectCategorie = ((item) => {
-    setValue(item.value)
-    setCategorie(false)
-  }, []);
+  const selectCategorie =
+    ((item) => {
+      setValue(item.value);
+      setCategorie(false);
+    },
+    []);
 
   return (
     <View style={styles.container}>
-      <Button title="Connexion / Inscription" onPress={() => navigation.navigate("ConnexionInscription")} />
+      <Button
+        title="Connexion / Inscription"
+        onPress={() => navigation.navigate("ConnexionInscription")}
+      />
       <View style={styles.dropdownInputs}>
         <TouchableOpacity style={styles.categorie} onPress={toggleCategorie}>
-          <Text>{value || 'Catégorie'}</Text>
-          <AntDesign name={categorie?'caretup':'caretdown'} size={12} />
+          <Text>{value || "Catégorie"}</Text>
+          <AntDesign name={categorie ? "caretup" : "caretdown"} size={12} />
         </TouchableOpacity>
         {categorie ? (
           <View style={styles.categoriesList}>
-          <FlatList keyExtractor={(item) => item.value}
-          data={[
-            {value: 'Catégorie 1'},
-            {value: 'Catégorie 2'},
-          ]}
-          renderItem={({item}) => (
-            <TouchableOpacity activeOpacity={0.8} style={styles.categorieItem} onPress={() => selectCategorie(item)}>
-              <Text>{item.value}</Text>
-            </TouchableOpacity>
-          )}
-          ItemSeparatorComponent={() => <View style={{height: 10}} />} />
-          </View> ) : null}
+            <FlatList
+              keyExtractor={(item) => item.value}
+              data={[{ value: "Catégorie 1" }, { value: "Catégorie 2" }]}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.categorieItem}
+                  onPress={() => selectCategorie(item)}
+                >
+                  <Text>{item.value}</Text>
+                </TouchableOpacity>
+              )}
+              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+            />
+          </View>
+        ) : null}
 
         <TouchableOpacity style={styles.tri} onPress={toggleTri}>
           <Text>Trier par</Text>
-          <AntDesign name={tri?'caretup':'caretdown'} size={12} />
+          <AntDesign name={tri ? "caretup" : "caretdown"} size={12} />
         </TouchableOpacity>
         {tri ? (
           <View style={styles.triList}>
-          <FlatList keyExtractor={(item) => item.value}
-          data={[
-            {value: 'Date'},
-            {value: 'Prix'},
-          ]}
-          renderItem={({item}) => (
-            <TouchableOpacity activeOpacity={0.8} style={styles.triItem}>
-              <Text>{item.value}</Text>
-            </TouchableOpacity>
-          )}
-          ItemSeparatorComponent={() => <View style={{height: 10}} />} />
-          </View> ) : null}
-
+            <FlatList
+              keyExtractor={(item) => item.value}
+              data={[{ value: "Date" }, { value: "Prix" }]}
+              renderItem={({ item }) => (
+                <TouchableOpacity activeOpacity={0.8} style={styles.triItem}>
+                  <Text>{item.value}</Text>
+                </TouchableOpacity>
+              )}
+              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+            />
+          </View>
+        ) : null}
       </View>
-      <ScrollView style={styles.scrollview}> 
-
-      </ScrollView>
+      <ScrollView style={styles.scrollview}></ScrollView>
     </View>
   );
 }
@@ -71,41 +86,41 @@ export default function PageAcceuilScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCEE',
-    justifyContent: 'space-around',
+    backgroundColor: "#F5FCEE",
+    justifyContent: "space-around",
   },
   dropdownInputs: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   categorie: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     width: 160,
     height: 40,
     borderWidth: 0.5,
-    borderRadius: 50
+    borderRadius: 50,
   },
   tri: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     width: 120,
     height: 40,
     borderWidth: 0.5,
-    borderRadius: 50
+    borderRadius: 50,
   },
   categorieItem: {
     height: 40,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   categoriesList: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     width: 160,
     padding: 10,
     borderRadius: 10,
@@ -114,13 +129,13 @@ const styles = StyleSheet.create({
   },
   triItem: {
     height: 40,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   triList: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     left: 240,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     width: 120,
     padding: 10,
     borderRadius: 10,
@@ -129,6 +144,6 @@ const styles = StyleSheet.create({
   },
   scrollview: {
     flex: 1,
-    padding: 25
+    padding: 25,
   },
 });
