0c8321cd6b12851d72cf6bf4aced74acbb4104f7
GGhezali <gregory.ghezali@hotmail.fr>
2025-04-16 09:46:58 +0200

    new commit

--- screens/Publier.js
+++ screens/Publier.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from "react-native";
+import { Button, StyleSheet, View, Text, SafeAreaView, Platform, StatusBar, TouchableOpacity } from "react-native";
 import Headers from "./Headers";
 import { useSelector } from "react-redux";
 
@@ -20,20 +20,35 @@ export default function PublierScreen({ navigation }) {
 
         <Headers navigation={navigation} isReturn={true} title={"Publier"} />
         <View style={styles.container}>
-
-        <View style={styles.containt}>
           
-        <Text style={styles.title}>Publier</Text>
-        <Button
-          title="Gallerie"
-          onPress={() => navigation.navigate("Gallerie")}
-        />
-        <Button title="Photo" onPress={() => navigation.navigate("Photo")} />
-        <Button
-          title="Publier"
-          onPress={() => handlePublish()}
-        />
-        </View>
+       <View style={styles.alignButtons}>
+       <View style={styles.button1}>
+          <TouchableOpacity
+            title="Gallerie"
+            onPress={() => navigation.navigate("Gallerie")}
+            >
+          <Text style={styles.textButton}>Gallerie</Text>
+          </TouchableOpacity>
+       </View>
+
+       <View style={styles.button1}>
+          <TouchableOpacity
+            title="Photo"
+            onPress={() => navigation.navigate("Photo")}
+            >
+          <Text style={styles.textButton}>Photo</Text>
+          </TouchableOpacity>
+       </View>
+       </View>   
+
+       <View style={styles.button2}>
+          <TouchableOpacity
+            title="Publier"
+            onPress={() => handlePublish()}
+          >
+          <Text style={styles.textButton}>Publier</Text>
+          </TouchableOpacity>
+       </View>
         </View>
       </SafeAreaView>
   );
@@ -45,15 +60,51 @@ const styles = StyleSheet.create({
       paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     },
   container: {
-    flex: 1,
+    width: "100%",
+    height: "100%",
     justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "#F5FCEE",
   },
   containt: {
-    flex: 1,
+    width: "100%",
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCEE",
-  }
+  },
+  alignButtons: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button1: {
+    backgroundColor: "#A0D9C1",
+    borderRadius: 30,
+    width: "40%",
+    height: "40",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  button2: {
+    backgroundColor: "#1C7C54",
+    borderRadius: 30,
+    width: "80%",
+    height: "40",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  textButton: {
+    justifyContent: "center",
+    textAlign: "center",
+    fontStyle: "bold",
+    fontSize: 20,
+    color: "white",
+    marginTop: 7,
+  },
 });
