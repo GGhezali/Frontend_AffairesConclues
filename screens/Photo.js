15d1158c22933f53cb792fe6cf328fc38836a4f5
GGhezali <gregory.ghezali@hotmail.fr>
2025-04-17 12:15:19 +0200

    new commit

--- screens/Photo.js
+++ screens/Photo.js
@@ -1,16 +1,45 @@
 import React from "react";
 import { Button, StyleSheet, View, Text, Platform, StatusBar  } from "react-native";
+import { CameraView, Camera } from 'expo-camera';
+import { useEffect, useState, useRef } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+
+
+
 
 export default function PhotoScreen({navigation}) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Photo</Text>
-      <Button title="Go back" onPress={() => navigation.navigate("Publier")} />
-      <Button
-        title="Gallerie"
-        onPress={() => navigation.navigate("Gallerie")}
-      />
-    </View>
+  
+  const [hasPermission, setHasPermission] = useState(false);
+  const cameraRef = useRef(null);
+  const isFocused = useIsFocused();
+  
+  useEffect(() => {
+    (async () => {
+      const result = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(result && result?.status === 'granted');
+    })();
+  }, []);
+
+
+  const takePicture = async () => {                 
+    const photo = await cameraRef.current?.takePictureAsync({ quality: 0.3 }); 
+    photo && console.log(photo.uri);         
+  };
+
+
+  if (!hasPermission || !isFocused) {
+    return <View />;
+  }
+
+  return (      
+      <CameraView style={{ flex: 1 }} ref={(ref) => (cameraRef.current = ref)}>
+        <Button title="Go back" onPress={() => navigation.navigate("Publier")} />
+        <Button
+          title="Gallerie"
+          onPress={() => navigation.navigate("Gallerie")}
+        />
+        <Button title="Prendre une photo !" onPress={() => takePicture()} />
+      </CameraView>
   );
 }
 
